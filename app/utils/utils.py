124495333a4f31c9ed899b822464f34b8f601950
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-11-16 18:44:44 +0300

    feat(utils): add method for managing resource paths

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,7 +1,23 @@
+import os
+import sys
+
+import keras
 import pandas as pd
 
+from datetime import timedelta
 
-def load_data_from_csv(file_path : str) -> pd.DataFrame:
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+
+def load_data_from_csv(file_path : str) -> pd.DataFrame | None:
     """Получить данные для отрисовки графика биржевых котировок из csv файла"""
     data = pd.read_csv(file_path)
     return data[['DateTime', 'Close']]
