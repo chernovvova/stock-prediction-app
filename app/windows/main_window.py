e9354dcfdfc3108b60f22f99a138424714f38079
Vladimir Chernov <chernovvova2023@yandex.ru>
2025-11-16 18:45:17 +0300

    feat(utils): add error window if data format is wrong

--- app/windows/main_window.py
+++ app/windows/main_window.py
@@ -1,8 +1,14 @@
 from PySide6.QtWidgets import (
-    QMainWindow, QWidget, QVBoxLayout, QHBoxLayout,
-    QPushButton, QFileDialog
+    QMainWindow,
+    QWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QPushButton,
+    QFileDialog,
+    QMessageBox,
 )
 
+from app.utils.utils import load_data_from_csv
 from app.widgets.plot_widget import PlotWidget
 
 
@@ -33,14 +39,26 @@ class MainWindow(QMainWindow):
         bottom_layout.addWidget(self.predict_button)
 
     def select_file(self):
+        """Выбор файла данных"""
         file_path, _ = QFileDialog.getOpenFileName(
             self,
             "Выберите данные",
             "",
             "Data files (*.csv *.txt *.xlsx *.xls)"
         )
-        if file_path:
-            self.plot_widget.load_from_file(file_path)
+
+        file_extension = file_path.split('.')[-1]
+
+        stock_data = None
+        if file_extension == 'csv':
+            try:
+                stock_data = load_data_from_csv(file_path)
+            except ValueError:
+                QMessageBox.warning(self, "Ошибка", "CSV файл должен содержать столбцы 'DateTime' и 'Close'")
+        else:
+            raise NotImplementedError(f'Файлы с расширением {file_extension} не поддерживаются.')
+
+        self.plot_widget.update_plot(data=stock_data)
 
     def make_prediction(self):
         self.plot_widget.add_prediction()
